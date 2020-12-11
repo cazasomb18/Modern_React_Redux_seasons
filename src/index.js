0093f3dec0bd298f5ab27d626c1fb6f3fb7d9d63
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-10 18:41:26 -0600

    creating repo for 3rd creat-react-app

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+	// constructor(props) {
+	// 	super(props);
+
+	// 	this.state = {lat: null, errorMessage: ''};
+
+	// }
+
+	state = {lat: null, errorMessage: ''};
+
+
+
+	componentDidMount(){
+
+		window.navigator.geolocation.getCurrentPosition(
+			position => this.setState({lat: position.coords.latitude}),
+			err => this.setState({ errorMessage: err.message })
+		);
+
+	}
+
+	componentDidUpdate(){
+
+		console.log('my component was just updated - it RERENDERED!');
+
+	}
+
+	render() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <div><SeasonDisplay lat={this.state.lat}/></div>
+		}
+
+		return <div>Loading...</div>
+	};
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
